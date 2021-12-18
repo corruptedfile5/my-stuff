2ad24834f07cfa66cd33739cdaf8a23af19a13d9
corruptedfile5 <96342169+corruptedfile5@users.noreply.github.com>
2021-12-18 17:06:11 +0100

    Add files via upload

---
+++ IP CHECKER FULL VERSION/IP CHECKER.bat
@@ -0,0 +1,9 @@
+@echo off
+title IP CHECKER
+color 0a
+echo This app will show you your IP ADDRESS information and more.
+echo A message will appear when it is done.
+pause
+ipconfig
+start Message.vbs
+pause
