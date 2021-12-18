e5af7f87e73b246cb608fe3dacc912c8efaee0cc
corruptedfile5 <96342169+corruptedfile5@users.noreply.github.com>
2021-12-18 15:34:41 +0100

    Add files via upload

---
+++ IP CHECKER.bat
@@ -0,0 +1,10 @@
+@echo off
+title IP CHECKER
+color 0a
+echo This app will show you your IP ADDRESS information and more.
+echo Do not share this info to anyone as through your ip address they can get your location.
+echo I do not own any responsibility for this if anything happens to your ip address.
+pause
+ipconfig
+echo That's all, scroll up to see the info.
+pause
